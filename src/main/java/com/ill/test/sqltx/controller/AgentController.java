7c05f17ad70cfd1c1716a4996e71079e9c539856
Robin Ellis <robin.cake.ellis@gmail.com>
2022-06-25 11:00:14 +0700

    controller

--- src/main/java/com/ill/test/sqltx/controller/AgentController.java
+++ src/main/java/com/ill/test/sqltx/controller/AgentController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ill.test.sqltx.repository.AgentRow;
@@ -20,25 +19,21 @@ public class AgentController {
     private AgentService service;
 
     @GetMapping("")
-    @ResponseBody
     public Flux<AgentRow> getAll() {
         return service.getAll();
     }
 
     @GetMapping("/ids")
-    @ResponseBody
     public Flux<String> getAllIds() {
         return service.getAll().map(agent -> String.valueOf(agent.getAgentId()) + '\n');
     }
 
     @GetMapping("/corp/{id}")
-    @ResponseBody
     public Flux<AgentRow> getForCorp(@PathVariable int id) {
         return service.getForCorp(id);
     }
 
     @GetMapping("/location/{id}")
-    @ResponseBody
     public Flux<AgentRow> getForLocation(@PathVariable int id) {
         return service.getForLocation(id);
     }
