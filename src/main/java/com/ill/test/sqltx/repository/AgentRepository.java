70d088afaa5dbdf2589835619398ba1276394cba
Robin Ellis <robin.cake.ellis@gmail.com>
2023-02-14 11:22:48 +0700

    update boot version

--- src/main/java/com/ill/test/sqltx/repository/AgentRepository.java
+++ src/main/java/com/ill/test/sqltx/repository/AgentRepository.java
@@ -1,11 +1,8 @@
 package com.ill.test.sqltx.repository;
 
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import org.springframework.stereotype.Repository;
-
 import reactor.core.publisher.Flux;
 
-@Repository
 public interface AgentRepository extends R2dbcRepository<AgentRow, Integer> {
 
     Flux<AgentRow> findAllByCorporationId(int corpId);
